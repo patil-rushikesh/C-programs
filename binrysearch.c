9517d08cb80af8ba3e4dd424174745e12963c778
Rushikesh Patil <69971167+patil-rushikesh@users.noreply.github.com>
2023-09-30 15:53:38 +0530

    Update binrysearch.c

--- binrysearch.c
+++ binrysearch.c
@@ -4,29 +4,47 @@ int main(){
     int a,n;
     printf("Enter the size of array\n");
     scanf("%d",&a);
-    printf("Enter the element to be searched\n");
-    scanf("%d",&n);
+  
     int l,r,m,data[a];
     printf("Enter %d elements (Make sure the elements are sorted)\n",a);
-    for (int i = 0; i < a; i++)
+     for (int i = 0; i < a; i++)
     {
         scanf("%d",&data[i]);
     }
+    printf("Enter the element to be searched\n");
+    scanf("%d",&n);
     
     l=0;
-    r=a;
-    while(l != r ){
-        m=(l+r)/2;
+    r=a-1;
+    while (l != r)
+    {
+         m=(l+r)/2;
+        if(data[l] == n){
+            l++;
+            printf("Element is at location %d",l);
+            exit(0);
+        }
+        if(data[r] == n){
+            r++;
+            printf("Element is at location %d",r);
+            exit(0);
+        }
         if( data[m] == n ){
             m++;
             printf("Element is at location %d",m);
             exit(0);
+
         }else if(data[m] > n){
-            l = m + 1;
-        }else{
+
             r = m - 1;
+            
+        }else if(data[m] < n){
+
+            l = m + 1;
+
         }
     }
+      
     printf("Element not found");
     return 0;
 }
