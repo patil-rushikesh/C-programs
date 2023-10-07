8cb7655e05dda8d412b9408d9f0e05bcbbbb4975
Rushikesh Patil <69971167+patil-rushikesh@users.noreply.github.com>
2023-10-07 09:59:20 +0530

    Create sorting.c

---
+++ sorting.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int a,temp,x;
+    printf("Enter the size of array\n");
+    scanf("%d",&a);
+    printf("Enter %d elements\n",a);
+    int data[a];
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&data[i]);
+    }
+    for(int k = 0; k<a ; k++){
+        x = data[k];
+        for(int j= 0; j<a; j++){
+            if(data[j] > data[k]){
+                temp = data[k];
+                data[k]=data[j];
+                data[j]= temp;
+            }
+    }
+    }
+    printf("Sorted Elements are : \n");
+    for(int k=0; k<a; k++){
+        printf("%d \n",data[k]);
+    }
+    
+}
