9f151d591e76012b637d52d6469cbd4309ee045a
Rushikesh Patil <69971167+patil-rushikesh@users.noreply.github.com>
2023-12-03 19:27:21 +0530

    Update bubblesortingforfloat.c

--- bubblesortingforfloat.c
+++ bubblesortingforfloat.c
@@ -1,28 +1,34 @@
 #include<stdio.h>
-int main(){
+
+int main() {
     int a;
-    float temp,x;
     printf("Enter the size of array\n");
-    scanf("%d",&a);
-    printf("Enter %d elements\n",a);
+    scanf("%d", &a);
+    
+    printf("Enter %d elements\n", a);
     float data[a];
-    for (int i = 0; i < a; i++)
-    {
-        scanf("%f",&data[i]);
-    }
-    for(int k = 0; k<a ; k++){
-        x = data[k];
-        for(int j= 0; j<a; j++){
-            if(data[j] < data[k]){
-                temp = data[k];
-                data[k]=data[j];
-                data[j]= temp;
-            }
-    }
-    }
-    printf("Sorted Elements are : \n");
-    for(int k=0; k<a; k++){
-        printf("%.3f \n",data[k]);
+    
+    for (int i = 0; i < a; i++) {
+        scanf("%f", &data[i]);
     }
     
+    // Perform Bubble Sort
+    for (int k = 0; k < a; k++) {
+        for (int j = 0; j < a - 1; j++) {
+            if (data[j] < data[j + 1]) {
+                // Swap elements
+                float temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }
+    }
+
+    // Print the sorted elements
+    printf("Sorted Elements are : \n");
+    for (int k = 0; k < a; k++) {
+        printf("%.3f\n", data[k]);
+    }
+
+    return 0;
 }
